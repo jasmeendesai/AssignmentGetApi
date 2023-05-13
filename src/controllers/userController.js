faf049cf9ece2f8c9ab70a1cc22b4d20cf64e723
jasmeendesai <jasmeendesai2597@gmail.com>
2023-05-14 00:32:45 +0530

    status-code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,50 +4,60 @@ const jwt = require("jsonwebtoken");
 
 
 const createUser= async function (req, res) {
-    let data= req.body
+    try {let data= req.body
     let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
+    res.status(201).send({msg: savedData})}
+    catch(error){
+      res.status(400).send({error : "An error occurred while creating the user's data."})
+    }
 }
 
 const userlogin= async function (req, res) {
-    const userName = req.body.emailId;
+    try{const userName = req.body.emailId;
     const password = req.body.password;
     const user = await UserModel.findOne({emailId : userName, password : password}) // object or null
     if(!user){
-        return res.send({status : false, msg : "username or the password is not correct"})
+        return res.status(401).send({status : false, msg : "username or the password is not correct"})
     }
     const token = jwt.sign({userId : user._id.toString()}, 'functionUp-tech');
     // console.log(token);
     res.setHeader("x-auth-header",token)
     // console.log(res.header["x-auth-header"]);
-    res.send({status : true, data:{token:token}});
+    res.status(201).send({status : true, data:{token:token}});}
+    catch(error){
+      res.status(500).send({error : "user login failed"})
+    }
 }
 
 const getUser= async function (req, res) {
-    const userId = req.params.userId;
+    try {const userId = req.params.userId;
     const userdetail= await UserModel.findById(userId);
-    res.send({status : true, data: {userdetail}});
+    res.status(201).send({status : true, data: {userdetail}});}
+    catch(error){
+      res.status(404).send({msg : "An error occurred while retrieving user's data."})
+    }
 }
 const updateUser= async function (req, res) {
-    const userId = req.params.userId;
+    try{const userId = req.params.userId;
     const data= req.body;
     const userdetail= await UserModel.findById(userId);
-    if(!userdetail) return res.send({msg :"User not found"})
-
+    if(!userdetail) return res.status(404).send({msg :"User not found"})
     const updatedUser= await UserModel.findOneAndUpdate({_id:userId},data,{new : true})
-    
-    res.send({updatedUser})
+    res.status(201).send({updatedUser})}
+    catch(error){
+      res.status(500).send({error : "Can not update user's data"})
+    }
 }
 
 const deleteUser= async function (req, res) {
-    const userId = req.params.userId;
- 
+    try{const userId = req.params.userId;
     const userdetail= await UserModel.findById(userId);
-    if(!userdetail) return res.send({msg :"User not found"})
-
+    if(!userdetail) return res.status(404).send({msg :"User not found"})
     const updatedUser= await UserModel.findOneAndUpdate({_id:userId},{$set :{isDeleted : true}},{new : true})
-    
-    res.send({updatedUser})
+    res.status(201).send({updatedUser})}
+    catch(error){
+      res.status(500).send({error : "Can not delete user's data"})
+    }
 }
 module.exports.createUser= createUser
 module.exports.userlogin= userlogin
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,25 +3,26 @@ const jwt = require("jsonwebtoken");
 
 
 const headerValidation = async function (req,res,next){
-    const auth= req.headers["x-auth-header"]; 
-    if(!auth) return res.send({status: false, msg : "error ,token is missing"});
-    // const validatejwt =jwt.verify(auth,'functionUp-technetium');
-    // if(!validatejwt) return res.send({ status: false, msg: "token is invalid" });
-    jwt.verify(auth,'functionUp-tech', (error,auth) => {
-        if(!auth) return res.send({ status: false, msg: "token is invalid" });
-    });
-    next();
+    try{const auth= req.headers["x-auth-header"]; 
+    if(!auth) return res.status(400).send({status: false, msg : "error ,token is missing"});
+
+    jwt.verify(auth,'functionUp-tech')
+    // (error,auth) => {
+    //     if(!auth) return res.status(401).send({ status: false, msg: "token is invalid" });
+    // });
+    next();}
+    catch(error){
+        res.status(401).send({ status: false, msg: "token is invalid" });
+    }
 }
-
-
 
 const authorise = function(req, res, next) {
     // comapre the logged in user's id and the id in request
-    let token = req.headers["x-auth-header"];
+   let token = req.headers["x-auth-header"];
     let decodedToken=jwt.verify(token, "functionUp-tech")
     let userToBeModified = req.params.userId
       let userLoggedIn =decodedToken.userId
-      if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
+      if(userToBeModified != userLoggedIn) return res.status(403).send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
     next()
 }
 module.exports.headerValidation=headerValidation;
